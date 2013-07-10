0edb6907684ee91f9312f799982fffb7468686d4
Viktor Gopanchuk <vitugan@gmail.com>
2013-07-10 13:56:28 +0000

    Mod1 web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var file = fs.readFileSync('index.html', 'utf-8', function(err, data){
+    if (err) throw err;
+    console.log(data);
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+    repsponse.send(file);
 });
 
 var port = process.env.PORT || 5000;
