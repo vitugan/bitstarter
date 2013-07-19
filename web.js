526931d916655c2cc3cc1f1898c7bd4dfb89fc14
Ubuntu <ubuntu@ip-172-31-20-217.us-west-2.compute.internal>
2013-07-19 07:38:55 +0000

    Resonsive index.html, web.js modified.

--- web.js
+++ web.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var file = fs.readFileSync('index.html', 'utf-8', function(err, data){
-    if (err) throw err;
-    console.log(data);
+
+app.get('/', function(request, responsse) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-    response.send(file);
-});
+var port = process.env.PORT || 8080;
+app.listen(port, function() { console.log("Listening on " + port); });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+
