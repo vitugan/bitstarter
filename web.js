2a222aa2d319446ce86d40d93fd60ffe29a704ad
Viktor Gopanchuk <vitugan@gmail.com>
2013-08-23 11:19:34 +0000

    Edit the error in web.js

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, responsse) {
+app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
     response.send(html);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 app.listen(port, function() { console.log("Listening on " + port); });
 
 
